0457713326b697e4466f70890fbbc854deb9f0e1
ubaid naeem <ubaid29170@gmail.com>
2025-08-16 17:16:19 +0500

    props with object

--- lec_17/src/App.jsx
+++ lec_17/src/App.jsx
@@ -1,55 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import User from './User'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import User from "./User";
 
 function App() {
+  let obj1 = {
+    name: "Rovik Danter",
+    email: "rovik.danter@test.com",
+    age: 24,
+  };
 
-    let obj = {
-      name: "Ubaid Naeem",
-      age: "20", 
-      email: "test@gmail.com"
-    }
-    let obj2 = {
-      name: "Ayesha Naeem",
-      age: "15", 
-      email: "ayesha@gmail.com"
-    }
-    let obj3 = {
-      name: "Salva Naeem",
-      age: "20", 
-      email: "salva@gmail.com"
-    }
-    let obj4 = {
-      name: "Eman Naeem",
-      age: "20", 
-      email: "salva@gmail.com"
-    }
-    let obj5 = {
-      name: "Sanaullah Naeem",
-      age: "20", 
-      email: "salva@gmail.com"
-    }
-    
+  let obj2 = {
+    name: "Mirel Vanton",
+    email: "mirel.vanton@test.com",
+    age: 29,
+  };
 
-    
+  let obj3 = {
+    name: "Kelvon Strak",
+    email: "kelvon.strak@test.com",
+    age: 21,
+  };
+
+  let obj4 = {
+    name: "Taren Lufor",
+    email: "taren.lufor@test.com",
+    age: 27,
+  };
+
+  let obj5 = {
+    name: "Zyric Mondal",
+    email: "zyric.mondal@test.com",
+    age: 26,
+  };
 
   return (
-    <> 
-    <h1>Props in React</h1>
+    <>
+      <h1>Props in React</h1>
 
-  
-
-    {/* <User obj={obj}/> */}
-    <User obj={obj}/>
-    <User obj={obj4}/>
-    <User obj={obj2}/>
-    <User obj={obj3}/>
-    <User obj={obj5}/>
-     
+      <User obj={obj1} />
+      <User obj={obj4} />
+      <User obj={obj2} />
+      <User obj={obj3} />
+      <User obj={obj5} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
